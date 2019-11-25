f45a00f7e62091764e86f81adf695f52fab52ae6
Sascha Peilicke <sascha@peilicke.de>
2019-11-25 11:36:31 +0100

    Fix Lint issue around equals in DiffCallback
    We'll take good care ;-)

--- presentation/common/src/main/kotlin/saschpe/gameon/common/recyclerview/DiffCallback.kt
+++ presentation/common/src/main/kotlin/saschpe/gameon/common/recyclerview/DiffCallback.kt
@@ -1,9 +1,11 @@
 package saschpe.gameon.common.recyclerview
 
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 
 class DiffCallback<T> : DiffUtil.ItemCallback<T>() {
     override fun areItemsTheSame(oldItem: T, newItem: T) = oldItem.hashCode() == newItem.hashCode()
 
+    @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: T, newItem: T) = oldItem == newItem
 }
