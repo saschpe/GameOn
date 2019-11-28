613955afae12c0c63d59e2e800ebc641216e5a4d
Sascha Peilicke <sascha@peilicke.de>
2019-11-28 10:22:58 +0100

    Directly link to the app's notification settings
    Instead of providing custom settings...

--- presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/settings/preferences/MainPreferenceFragment.kt
+++ presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/settings/preferences/MainPreferenceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import saschpe.gameon.common.content.openAppNotificationSettings
 import saschpe.gameon.mobile.BuildConfig
 import saschpe.gameon.mobile.R
 import saschpe.gameon.mobile.settings.SettingsViewModel
@@ -23,6 +24,11 @@ class MainPreferenceFragment : PreferenceFragmentCompat() {
             findPreference<Preference>(getString(R.string.pref_developer_key))?.isVisible = false
         }
 
+        findPreference<Preference>(getString(R.string.pref_notification_key))?.setOnPreferenceClickListener {
+            requireContext().openAppNotificationSettings()
+            true
+        }
+
         findPreference<Preference>(getString(R.string.pref_help_and_feedback_key))?.setOnPreferenceClickListener {
             findNavController().navigate(R.id.action_settingsFragment_to_helpFragment)
             true
@@ -31,7 +37,6 @@ class MainPreferenceFragment : PreferenceFragmentCompat() {
         // Workaround for https://issuetracker.google.com/issues/139995974
         listOf(
             R.string.pref_appearance_key,
-            R.string.pref_notification_key,
             R.string.pref_developer_key
         ).forEach {
             findPreference<Preference>(getString(it))?.setOnPreferenceClickListener(
