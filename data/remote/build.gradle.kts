7f973061979819aae91b95c6945415574b203b4a
Sascha Peilicke <sascha@peilicke.de>
2019-11-29 10:13:28 +0100

    Move asResult to top-level into :data:core
    A more generic version of the previous RemoteRepository.asResult method.
    Remove that marker interface now that it's not necessary any more. Since
    the behavior of asResult is properly tested, call-sites don't need to
    specify return explicitly any more.

--- data/remote/build.gradle.kts
+++ data/remote/build.gradle.kts
@@ -16,6 +16,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0")
 
-    testImplementation("junit:junit:4.12")
-    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation(kotlin("test-junit", "1.3.61"))
 }
