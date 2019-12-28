4b5a26ed6de695ef8393ed51f8a36a6739bb3e6d
Sascha Peilicke <sascha@peilicke.de>
2019-12-28 18:01:22 +0100

    Proper price threshold update
    Doing it in onPause was bad, as the ViewModel paused as well and the update
    never happened. Use a dedicated TextWatcher and only programmatically set
    when there is no focus (i.e. when the user didn't trigger a text change).
    
    Lastly, change the icon if the user set an alarm to give a visual clue.

--- presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/game/overview/GameOverviewFragment.kt
+++ presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/game/overview/GameOverviewFragment.kt
@@ -1,19 +1,49 @@
 package saschpe.gameon.mobile.game.overview
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat.getDrawable
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.fragment_game_overview.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import saschpe.gameon.mobile.R
 import saschpe.gameon.mobile.base.customtabs.CustomTabs.openUrl
 
 class GameOverviewFragment : Fragment(R.layout.fragment_game_overview) {
     private lateinit var paramPlain: String
     private val viewModel: GameOverviewViewModel by viewModels()
+    private var priceAlertTextWatcher = object : TextWatcher {
+        private var currentJob: Job? = null
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+        override fun afterTextChanged(text: Editable?) {
+            updatePriceAlertStartIcon()
+
+            currentJob?.cancel() // Only have one, i.e. the latest, running...
+            currentJob = viewLifecycleOwner.lifecycleScope.launch {
+                delay(200L) // Rate-limit to avoid an update on every key press...
+
+                viewModel.favoriteLiveData.value?.let {
+                    var priceAlertString = text?.toString()
+                    if (priceAlertString?.isBlank() == true) {
+                        priceAlertString = null
+                    }
+                    viewModel.updateFavorite(it.copy(priceThreshold = priceAlertString?.toDouble()))
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -91,7 +121,11 @@ class GameOverviewFragment : Fragment(R.layout.fragment_game_overview) {
                     favoriteButton.setOnClickListener { viewModel.removeFavorite(favorite.plain) }
 
                     priceAlertGroup.visibility = View.VISIBLE
-                    favorite.priceThreshold?.let { priceAlertInput.setText(it.toString()) }
+                    if (!priceAlertInput.hasFocus()) {
+                        favorite.priceThreshold?.let { priceAlertInput.setText(it.toString()) }
+                    }
+                    priceAlertInput.addTextChangedListener(priceAlertTextWatcher)
+                    updatePriceAlertStartIcon()
                 } else {
                     favoriteButton.icon = getDrawable(R.drawable.ic_favorite_border_24dp)
                     favoriteButton.text = getString(R.string.add_to_favorites)
@@ -103,14 +137,11 @@ class GameOverviewFragment : Fragment(R.layout.fragment_game_overview) {
         })
     }
 
-    override fun onPause() {
-        super.onPause()
-        viewModel.favoriteLiveData.value?.let {
-            var priceAlertString = priceAlertInput.text?.toString()
-            if (priceAlertString?.isBlank() == true) {
-                priceAlertString = null
-            }
-            viewModel.updateFavorite(it.copy(priceThreshold = priceAlertString?.toDouble()))
+    private fun updatePriceAlertStartIcon() {
+        priceAlertInputLayout.startIconDrawable = if (priceAlertInput.text?.isEmpty() == true) {
+            getDrawable(resources, R.drawable.ic_alarm_24dp, null)
+        } else {
+            getDrawable(resources, R.drawable.ic_alarm_on_24dp, null)
         }
     }
 
