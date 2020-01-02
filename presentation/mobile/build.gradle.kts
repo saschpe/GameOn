38e4f46d3f1f460da9a3c55a8fa1a8f5a5cb95ce
Sascha Peilicke <sascha@peilicke.de>
2020-01-02 17:53:07 +0100

    Update Coil to 0.9.1
    See https://github.com/coil-kt/coil/blob/master/CHANGELOG.md#091---december-30-2019

--- presentation/mobile/build.gradle.kts
+++ presentation/mobile/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics:17.2.1")
     implementation("com.google.firebase:firebase-common-ktx:19.3.0")
     implementation("com.google.android.material:material:1.1.0-rc01")
-    implementation("io.coil-kt:coil:0.8.0")
+    implementation("io.coil-kt:coil:0.9.1")
     implementation("me.zhanghai.android.materialprogressbar:library:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
     implementation("saschpe.android:customtabs:3.0.1")
