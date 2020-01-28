36ddd3456301b1d4b5f309d1e670087538a1bfdd
Sascha Peilicke <sascha@peilicke.de>
2020-01-28 19:56:32 +0100

    Allow all stores in game overview
    Fixes #10

--- data/remote/src/main/kotlin/saschpe/gameon/data/remote/repository/GameRemoteRepository.kt
+++ data/remote/src/main/kotlin/saschpe/gameon/data/remote/repository/GameRemoteRepository.kt
@@ -15,6 +15,8 @@ class GameRemoteRepository(
 ) {
     /**
      * Get info about game.
+     *
+     * @see <a href="https://itad.docs.apiary.io/#reference/game/info/get-info-about-game">API documentation</a>
      */
     suspend fun info(plains: List<String>) = asResult {
         api.get<GameInfoResponse>("game/info") {
@@ -24,25 +26,29 @@ class GameRemoteRepository(
 
     /**
      * Get basic information about game's prices: best current price, historical lowest price, and bundles in which the game is included.
+     *
+     * @see <a href="https://itad.docs.apiary.io/#reference/game/info/get-game-price-overview">API documentation</a>
      */
     suspend fun overview(
         plains: List<String>,
         region: String = "eu1",
         country: String = "de",
-        shops: List<String> = listOf(),
-        allowed: List<String> = listOf("steam", "gog")
+        shop: String = "stream",
+        allowed: List<String> = listOf()
     ) = asResult {
         api.get<GameOverviewResponse>("game/overview") {
             parameter("plains", plains.joinToString(separator = ","))
             parameter("region", region)
             parameter("country", country)
-            parameter("shops", shops.joinToString(separator = ","))
+            parameter("shop", shop)
             parameter("allowed", allowed.joinToString(separator = ","))
         }
     }
 
     /**
      * Get all current prices for one or more selected games. Use region and country to get more accurate results.
+     *
+     * @see <a href="https://itad.docs.apiary.io/#reference/game/prices/get-current-prices">API documentation</a>
      */
     suspend fun prices(
         plains: List<String>,
