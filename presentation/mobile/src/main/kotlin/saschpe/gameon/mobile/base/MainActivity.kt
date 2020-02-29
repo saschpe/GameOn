4a3131476e0202d286a395602553133f96a7742e
Sascha Peilicke <sascha@peilicke.de>
2020-02-29 18:23:31 +0100

    Load shared preferences in IO context

--- presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/base/MainActivity.kt
+++ presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/base/MainActivity.kt
@@ -3,10 +3,14 @@ package saschpe.gameon.mobile.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import saschpe.gameon.common.content.sharedPreferences
 import saschpe.gameon.mobile.R
 
@@ -22,8 +26,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onStart() {
         super.onStart()
-        bottomNavigationView.selectedItemId =
-            sharedPreferences.getInt(PREF_SELECTED_ITEM, R.id.favoritesFragment)
+        lifecycleScope.launch {
+            bottomNavigationView.selectedItemId = withContext(Dispatchers.IO) {
+                sharedPreferences.getInt(PREF_SELECTED_ITEM, R.id.favoritesFragment)
+            }
+        }
     }
 
     override fun onStop() {
