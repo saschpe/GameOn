418c969005fb359ddb6a47d7420982dfb9c5645a
Sascha Peilicke <sascha@peilicke.de>
2020-02-29 17:13:44 +0100

    Add profile screens

---
+++ presentation/mobile/src/main/kotlin/saschpe/gameon/mobile/base/text/TextInputLayoutDisableErrorTextWatcher.kt
@@ -0,0 +1,17 @@
+package saschpe.gameon.mobile.base.text
+
+import android.text.Editable
+import android.text.TextWatcher
+import com.google.android.material.textfield.TextInputLayout
+
+class TextInputLayoutDisableErrorTextWatcher(
+    private val inputLayout: TextInputLayout?
+) : TextWatcher {
+    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+        inputLayout?.isErrorEnabled = false
+    }
+
+    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) = Unit
+
+    override fun afterTextChanged(editable: Editable) = Unit
+}
