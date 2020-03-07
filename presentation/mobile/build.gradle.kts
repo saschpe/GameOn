52335237fefa384c5a8b784642ceaf063416b8b9
Sascha Peilicke <sascha@peilicke.de>
2020-03-07 09:22:59 +0100

    Kotlin: Update Kotlinx.Coroutines to 1.3.4
    See https://github.com/Kotlin/kotlinx.coroutines/releases/tag/1.3.4
    i# On branch updates

--- presentation/mobile/build.gradle.kts
+++ presentation/mobile/build.gradle.kts
@@ -101,7 +101,7 @@ dependencies {
     implementation("com.google.android.material:material:1.1.0")
     implementation("io.coil-kt:coil:0.9.5")
     implementation("me.zhanghai.android.materialprogressbar:library:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.4")
     implementation("saschpe.android:customtabs:3.0.2")
     implementation("saschpe.android:social-fragment:2.0.3")
     implementation("saschpe.android:versioninfo:2.1.2")
