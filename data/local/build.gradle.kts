cf002194477c162b3a9ee3e749ef13ff2dead23e
Sascha Peilicke <sascha@peilicke.de>
2020-03-20 14:21:59 +0100

    Kotlin: Update Coroutines to 1.3.5
    See https://github.com/Kotlin/kotlinx.coroutines/releases/tag/1.3.5

--- data/local/build.gradle.kts
+++ data/local/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation(project(":data:core"))
     implementation("androidx.room:room-runtime:2.2.5")
     implementation("androidx.room:room-ktx:2.2.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
 
     testImplementation("androidx.room:room-testing:2.2.5")
     testImplementation("androidx.test.ext:junit-ktx:1.1.1")
