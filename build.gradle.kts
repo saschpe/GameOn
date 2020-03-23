b137464bde213482d7556f11cc5bc89747ac792c
Sascha Peilicke <sascha@peilicke.de>
2020-03-23 20:28:29 +0100

    Kotlin: Update to 1.3.71
    Bug fixes, see [0].
    
    [0] https://github.com/JetBrains/kotlin/releases/tag/v1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 plugins {
     id("com.diffplug.gradle.spotless") version "3.27.2"
     id("com.github.ben-manes.versions") version "0.28.0"
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.3.71"
 }
 
 repositories {
