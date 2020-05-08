fba10aeb661453718df4f886d9c8dc7c2223f2ce
Sascha Peilicke <sascha@peilicke.de>
2020-05-08 14:04:01 +0200

    Gradle: Update Spotless plugin to 3.29.0
    See https://github.com/diffplug/spotless/blob/master/CHANGES.md#1290---2020-05-05

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 }
 
 plugins {
-    id("com.diffplug.gradle.spotless") version "3.28.1"
+    id("com.diffplug.gradle.spotless") version "3.29.0"
     id("com.github.ben-manes.versions") version "0.28.0"
     kotlin("jvm") version "1.3.72"
 }
