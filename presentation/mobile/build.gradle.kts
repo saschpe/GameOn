64bd65cd96fb443df775c1f9dc067dff27ec08f9
Sascha Peilicke <sascha@peilicke.de>
2020-05-28 09:54:00 +0200

    Release version 0.3.26
    Signed-off-by: Sascha Peilicke <sascha@peilicke.de>

--- presentation/mobile/build.gradle.kts
+++ presentation/mobile/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "saschpe.gameon"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 210000325
-        versionName = "0.3.25"
+        versionCode = 210000326
+        versionName = "0.3.26"
         multiDexEnabled = true
         base.archivesBaseName = "$applicationId-mobile-$versionName"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
