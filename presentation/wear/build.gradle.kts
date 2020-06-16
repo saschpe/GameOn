0a85b03fa070ee92494be579752fe7bb41a2af83
Sascha Peilicke <sascha@peilicke.de>
2020-06-16 07:57:44 +0200

    Release version 0.3.30
    Signed-off-by: Sascha Peilicke <sascha@peilicke.de>

--- presentation/wear/build.gradle.kts
+++ presentation/wear/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "saschpe.gameon"
         minSdkVersion(28)
         targetSdkVersion(29)
-        versionCode = 211000329
-        versionName = "0.3.29"
+        versionCode = 211000330
+        versionName = "0.3.30"
         base.archivesBaseName = "$applicationId-wear-$versionName"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
