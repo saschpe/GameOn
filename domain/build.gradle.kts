3345c0d6c9dc98d58ad42dfffa517043b7ceb2fc
Sascha Peilicke <sascha@peilicke.de>
2020-08-18 17:10:35 +0200

    Update kotlinx.coroutines to 1.3.9
    See https://github.com/Kotlin/kotlinx.coroutines/releases/tag/1.3.9

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     api(project(":data:core")) {
         // TODO: Consider domain-specific models to hide underlying layers. Currently, considered overkill.
     }
--- presentation/mobile/build.gradle.kts
+++ presentation/mobile/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     implementation("com.google.android.material:material:1.2.0")
     implementation("io.coil-kt:coil:0.11.0")
     implementation("me.zhanghai.android.materialprogressbar:library:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("saschpe.android:customtabs:3.0.2")
     implementation("saschpe.android:social-fragment:2.0.3")
     implementation("saschpe.android:versioninfo:2.1.2")
