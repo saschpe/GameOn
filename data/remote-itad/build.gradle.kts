f174aed2280cd0f66e84c01d3147a94550c8f81d
Sascha Peilicke <sascha@peilicke.de>
2020-08-20 08:29:23 +0200

    Update Ktor to 1.4.0
    See https://github.com/ktorio/ktor/releases/tag/1.4.0

--- data/remote-itad/build.gradle.kts
+++ data/remote-itad/build.gradle.kts
@@ -10,14 +10,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":data:core"))
-    implementation("io.ktor:ktor-client-android:1.3.2")
-    implementation("io.ktor:ktor-client-logging-jvm:1.3.2")
-    implementation("io.ktor:ktor-client-serialization-jvm:1.3.2")
+    implementation("io.ktor:ktor-client-android:1.4.0")
+    implementation("io.ktor:ktor-client-logging-jvm:1.4.0")
+    implementation("io.ktor:ktor-client-serialization-jvm:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
 
     testImplementation(kotlin("test-junit"))
-    testImplementation("io.ktor:ktor-client-mock-jvm:1.3.2")
+    testImplementation("io.ktor:ktor-client-mock-jvm:1.4.0")
     testImplementation("io.mockk:mockk:1.10.0")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
 }
