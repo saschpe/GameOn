77b3c8522fb3aa99479ec51b2a8a0ad04a4dd871
Sascha Peilicke <sascha@peilicke.de>
2021-02-25 15:30:08 +0100

    Update Ktor to 1.5.1

--- data/remote-itad/build.gradle.kts
+++ data/remote-itad/build.gradle.kts
@@ -10,14 +10,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(project(":data:core"))
-    implementation("io.ktor:ktor-client-android:1.4.0")
-    implementation("io.ktor:ktor-client-logging-jvm:1.4.0")
-    implementation("io.ktor:ktor-client-serialization-jvm:1.4.0")
+    implementation("io.ktor:ktor-client-android:1.5.1")
+    implementation("io.ktor:ktor-client-logging-jvm:1.5.1")
+    implementation("io.ktor:ktor-client-serialization-jvm:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
 
     testImplementation(kotlin("test-junit"))
-    testImplementation("io.ktor:ktor-client-mock-jvm:1.4.0")
+    testImplementation("io.ktor:ktor-client-mock-jvm:1.5.1")
     testImplementation("io.mockk:mockk:1.10.0")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
 }
